ee616cc9b31b93caf623069dfd2d421b293fea3b
shaimeur <haimeursaad@gmail.com>
2025-03-08 04:14:37 +0000

    The fundamentals of php the good waygit add .

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+</head>
+<body>
+<?php $bookName = "The Clean Coder" ;?>
+
+
+
+<h1> I'm reading "<?php echo $bookName ;  ?>"</h1>
+
+</body>
+</html>

b353e1a947aac29562118157827873f595780c9f
shaimeur <haimeursaad@gmail.com>
2025-03-08 04:39:43 +0000

    conditianals rendring

--- index.php
+++ index.php
@@ -6,11 +6,20 @@
     <title>Document</title>
 </head>
 <body>
-<?php $bookName = "The Clean Coder" ;?>
+<?php $bookName = "The Clean Coder" ;
+        $read = false ;
 
+?>
 
+<?php if ($read) { ?>
+ <h1> I'm reading "<?php echo $bookName ;  ?>"</h1>
 
-<h1> I'm reading "<?php echo $bookName ;  ?>"</h1>
+<?php  } else {?>
+
+<h1> I did not read "<?php echo  $bookName ;?>" </h1>
+
+<?php } ?>
+
 
 </body>
 </html>

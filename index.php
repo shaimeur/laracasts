2d51c8f01984c819d6b218e04a8441d83a6c3034
shaimeur <haimeursaad@gmail.com>
2025-03-11 04:13:22 +0000

    refactoring the code with a function to filter books by the name of the author

--- index.php
+++ index.php
@@ -13,25 +13,49 @@
 <?php
 
     $books = [
-                ['bookName' => "The Clean Coder", 'author' => "John Doe",'price' => 1000],
-                ['bookName' => 'Uml the good practice', 'author' => "Nick larcen",'price' => 2000],
-                ['bookName' => 'La formule de dieu !!', 'author' => "John Red",'price' => 3000],
+                [
+                        'bookName' => "The Clean Coder",
+                        'author' => "John Doe",
+                         'price' => 1000,
+                        'realseYear' => 2003
+                ],
+                [
+                        'bookName' => 'Uml the good practice',
+                        'author' => "Nick larcen",
+                        'price' => 2000,
+                        'realseYear' => 2003
+                ],
+                [
+                        'bookName' => 'La formule de dieu !!',
+                         'author' => "John Red",
+                         'price' => 3000,
+                         'realseYear' => 2003
+                ],
             ] ;
 
-
+    function filterBooksByAuthor($books,$author){
+    foreach ($books as $book ){
+        if($book['author'] == $author) {
+      return $book["bookName"] . " " . $book["price"];
+            }
+      }
+    }
 
 ?>
 
 <ul>
 
-    <?php
-        foreach ($books as $book)  : ?>
-            <li> <?= $book["bookName"]  , "Price " .$book["price"] ?></li>
-       <?php endforeach ?>
+
+
+
+            <li> <?=  filterBooksByAuthor($books,"John Red")?></li>
+
+
 
 
 </ul>
 
-<?=  $books[1]["price"] ; ?>
+
+
 </body>
 </html>

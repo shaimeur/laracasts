8379e2efa1c03cc5545812da637008f605c3b059
shaimeur <haimeursaad@gmail.com>
2025-11-08 20:11:34 +0100

    making the content of the banner dynamic using a the variable heading in every file (controllers) so we can print it in the html

--- about.php
+++ about.php
@@ -1,4 +1,7 @@
 <?php
 
+$heading = 'About' ;
+
+
 require "./views/about.view.php" ;
 
--- views/contact.view.php
+++ views/contact.view.php
@@ -8,7 +8,7 @@
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-     <p class="text-white">Hello Welcome to the Contact!!</p>
+     <p class="text-white">Hello Welcome to the Contact-US!!</p>
     </div>
   </main>
 <?php include('partials/footer.php')?>

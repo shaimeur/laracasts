13e07b1f93e73d7f010f550773459c93a980f11e
shaimeur <haimeursaad@gmail.com>
2025-11-09 18:00:02 +0100

    introduction to the var_dump function and the die() function so we do have to combine for debbugging purposes we have created a new function called dd() die and dump means kill excution with the die(à function dump is to print the value in the var_dump function

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 $heading = 'Home' ;
 
+function dd($value){
+
+    echo "<pre>" ;
+    var_dump($value);
+    echo "</pre>" ;
+    die();
+}
+echo $_SERVER["REQUEST_URI"] ;
+
+// dd($_SERVER);
+
 require "./views/index.view.php";
 
 
